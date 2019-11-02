a1e5a6ace497326b0e4e822a76ff9ac4461a6250
Jbkey1983 <jbk_1_tn@yahoo.com>
2019-11-01 21:51:35 -0500

    added logout event

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -5,11 +5,14 @@ import '../styles/main.scss';
 import auth from './components/Auth/auth';
 import apiKeys from './helpers/apiKeys.json';
 import authData from './helpers/data/authData';
+import logout from './components/MyNavbar/myNavbar';
+
 
 const init = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
   auth.loginButton();
   authData.checkLoginStatus();
+  logout.logoutEvent();
 };
 
 init();
