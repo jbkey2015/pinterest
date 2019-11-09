4b5c380147351a22453eaec1331cd3de248b293c
Jbkey1983 <jbk_1_tn@yahoo.com>
2019-11-09 13:28:57 -0600

    go back button works

--- src/javascripts/components/SingleBoard/singleBoard.js
+++ src/javascripts/components/SingleBoard/singleBoard.js
@@ -3,6 +3,7 @@ import util from '../../helpers/utilities';
 import pinData from '../../helpers/data/pinData';
 import pin from '../Pins/pins';
 
+
 const allBoards = $('#boards');
 const pinsBoard = $('#pinBoards');
 
@@ -11,6 +12,7 @@ const buildBoard = (boardId) => {
   allBoards.addClass();
   pinsBoard.removeClass();
   let domString = `<h1 class="text-center">${boardId.name}</h1>`;
+  domString += '<center><a href="/"class="btn btn-primary">Go Back</a></center>';
   domString += '<div id="board-section" class="d-flex flex-wrap justify-content-center">';
   pinData.getPins(boardId)
     .then((pins) => {
