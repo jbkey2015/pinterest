e21441e2ee9d4d94fd7c44e6e1b67efbb2fb737d
Jbkey1983 <jbk_1_tn@yahoo.com>
2019-11-09 10:09:22 -0600

    boards print to page

--- src/javascripts/components/boards/boards.js
+++ src/javascripts/components/boards/boards.js
@@ -1,1 +1,21 @@
-// <h1>Boards</h1>;
+import boardsData from '../../helpers/data/boardData';
+import utilities from '../../helpers/utilities';
+
+const showAllBoards = (uid) => {
+  boardsData.getBoards(uid)
+    .then((boards) => {
+      let domString = '';
+      domString += '<div id="myBoards" class="d-flex flex-wrap">';
+      boards.forEach((board) => {
+        domString += `
+        <div class="card col-4">
+          <p>${board.name}</p>
+        </div>
+        `;
+        utilities.printToDom('boards', domString);
+      });
+    })
+    .catch((error) => console.error(error));
+};
+
+export default { showAllBoards };
---
+++ src/javascripts/helpers/data/boardData.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import apiKeys from '../apiKeys.json';
+
+const baseurl = apiKeys.firebaseKeys.databaseURL;
+
+
+const getBoards = (uid) => new Promise((resolve, reject) => {
+  axios.get(`${baseurl}/boards.json?orderBy="uid"&equalTo="${uid}"`)
+    .then((response) => {
+      const demBoards = response.data;
+      const boards = [];
+      Object.keys(demBoards).forEach((fbId) => {
+        demBoards[fbId].id = fbId;
+        boards.push(demBoards[fbId]);
+      });
+      resolve(boards);
+    })
+    .catch((error) => reject(error));
+});
+
+export default { getBoards };
