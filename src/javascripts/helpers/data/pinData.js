5e86fbc44a1b6ff766b898ca2507edd6bb7ad21b
Jbkey1983 <jbk_1_tn@yahoo.com>
2019-11-24 14:08:56 -0600

    creating data for update

--- src/javascripts/helpers/data/pinData.js
+++ src/javascripts/helpers/data/pinData.js
@@ -18,9 +18,25 @@ const getPins = (boardId) => new Promise((resolve, reject) => {
     .catch((error) => reject(error));
 });
 
+const getPinById = (pinId) => new Promise((resolve, reject) => {
+  axios.get(`${baseurl}/pins/${pinId}.json`)
+    .then((response) => {
+      resolve(response.data);
+    })
+    .catch((err) => reject(err));
+});
+
 const deletePin = (pinId) => axios.delete(`${baseurl}/pins/${pinId}.json`);
 
 const addNewPin = (newPin) => axios.post(`${baseurl}/pins.json`, newPin);
 
+const updatePin = (pinId, edPin) => axios.put(`${baseurl}/pins/${pinId}.json`, edPin);
 
-export default { getPins, deletePin, addNewPin };
+
+export default {
+  getPins,
+  deletePin,
+  addNewPin,
+  getPinById,
+  updatePin,
+};
